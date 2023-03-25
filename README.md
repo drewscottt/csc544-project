75aebdb467bd852d32f4fced4d3b6356c5dadcc7
drewscottt <drewscott1103@gmail.com>
2023-03-25 16:59:28 -0700

    finish filtering, add json metadata

--- README.md
+++ README.md
@@ -10,7 +10,13 @@ This project's goal is to identify misleading data visualizations in the sports 
 * reddit_posts.py -- utilizes the Reddit API to retreive images associated with posts from various sports related subreddits
 * /subreddits -- contains directories corresponding to all the the subreddits that were accessed through the Reddit API; each sub-directory contains all of the images gathered
 * /filtered_subreddits -- contains directories corresponding to all the the subreddits that were accessed through the Reddit API; each sub-directory contains images that appear to be sufficiently related to "data visualization"
+* image_metadata.json -- maps image filename to image url and Reddit post ID, so that images can be tracked back to their source easily
 
 ### Usage
 
 In order to run ```python3 reddit_posts.py``` to collect the data, you will have to create your own authorized Reddit application [here](https://www.reddit.com/prefs/apps). Then, create a file ```local_secrets.py``` and create the variables needed to interact with the Reddit API.
+
+### Stats
+
+* 2,029 images were collected across all of the subreddits.
+* Then, 268 images were selected during the filter stage.
--- reddit_posts.py
+++ reddit_posts.py
@@ -3,14 +3,17 @@ import requests
 import re
 from pathlib import Path
 import os
+import json
 from local_secrets import REDDIT_SECRET, REDDIT_CLIENT_ID, REDDIT_USER_AGENT
 
+# set up reddit client app
 reddit = praw.Reddit(
     client_id=REDDIT_CLIENT_ID,
     client_secret=REDDIT_SECRET,
     user_agent=REDDIT_USER_AGENT,
 )
 
+# subreddits to read from
 sports_subreddits = [
     "CollegeBasketball",
     "baseball",
@@ -34,13 +37,20 @@ teams_subreddits = [
 
 all_subreddits = [*sports_subreddits, *teams_subreddits]
 
-for subreddit_name in teams_subreddits:
+# track metadata for each image read
+image_metadata = {}
+
+# read all of the "Hot" posts from each subreddit
+# extract the images from the posts
+for subreddit_name in all_subreddits:
+    print(subreddit_name)
     Path(os.path.join("subreddits", subreddit_name)).mkdir(parents=True, exist_ok=True)
 
     # credit: https://www.reddit.com/r/learnpython/comments/5benxs/how_do_i_download_an_image_using_praw/
     subreddit_instance = reddit.subreddit(subreddit_name)
     posts = subreddit_instance.hot(limit=None)
     for post in posts:
+        # we only care about posts that are images
         if post.is_reddit_media_domain and not post.is_video:
             url = (post.url)
             file_name = url.split("/")
@@ -50,6 +60,19 @@ for subreddit_name in teams_subreddits:
             if "." not in file_name:
                 file_name += ".jpg"
 
-            r = requests.get(url)
-            with open(os.path.join("subreddits", subreddit_name, file_name), "wb") as f:
-                f.write(r.content)
+            image_metadata[file_name] = {"url": post.url, "post_id": str(post)}
+
+            image_filename = os.path.join("subreddits", subreddit_name, file_name)
+            if not os.path.isfile(image_filename):
+                r = requests.get(url)
+                with open(image_filename, "wb") as f:
+                    f.write(r.content)
+
+# update the image_metadata.json file
+with open("image_metadata.json", "r") as f:
+    existing_json = json.load(f)
+
+existing_json.update(image_metadata)
+
+with open("image_metadata.json", "w") as f:
+    json.dump(existing_json, f)
